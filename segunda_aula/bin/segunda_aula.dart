ba83ce86e313f84b2c315a5022eac1d4756bf28f
H4ns81 <die.pfeife@hotmail.com>
2021-06-20 00:44:44 -0300

    Estrutura If/Else multiplos

--- segunda_aula/bin/segunda_aula.dart
+++ segunda_aula/bin/segunda_aula.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
+  /**
   print('Informe sua nota:');
   dynamic entradaNota = stdin.readLineSync();
   dynamic nota = double.parse(entradaNota);
@@ -9,6 +10,34 @@ void main(List<String> arguments) {
     print('voce reprovou! faltou $notaQueFaltou');
   } else {
     print('Você passou! Parabéns!');
+  }
+  */
+
+  print('Informe a sua nota:');
+  dynamic entradaNota = stdin.readLineSync();
+  dynamic nota = double.parse(entradaNota);
+
+  print('Informe o % de sua presença:');
+  dynamic entradaPresenca = stdin.readLineSync();
+  dynamic presenca = int.parse(entradaPresenca);
+
+  //Operadores lógicos -> && ou ||
+
+  if (presenca >= 75) {
+    if (nota >= 6) {
+      print('Aprovado!');
+    } else {
+      print('Informe a nota da recuperação:');
+      dynamic entradaRecuperacao = stdin.readLineSync();
+      dynamic recuperacao = double.parse(entradaRecuperacao);
+      if (recuperacao >= 6) {
+        print('Aprovado na recuperação!');
+      } else {
+        print('Reprovado na Recuperação!');
+      }
+    }
+  } else {
+    print('Reprovado pela frequência!');
   }
 }
 
