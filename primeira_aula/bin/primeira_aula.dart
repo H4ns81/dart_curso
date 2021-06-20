5069342de1991d903f6f2193ffca466471dfaca7
H4ns81 <die.pfeife@hotmail.com>
2021-06-19 23:35:55 -0300

    Trabalho com variaveis

--- primeira_aula/bin/primeira_aula.dart
+++ primeira_aula/bin/primeira_aula.dart
@@ -1,5 +1,27 @@
 import 'package:primeira_aula/primeira_aula.dart' as primeira_aula;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${primeira_aula.calculate()}!');
+  /**
+   * trabalhando com variaveis primeira opcao nomeando diretamente
+   */
+
+  //Primeiro exemplo
+  /* 
+  String nome = 'Joao';
+  int idade = 40;
+  double salario = 499.99;
+  */
+
+  //Segundo exemplo
+  print('Informe seu nome:');
+  var nome = stdin.readLineSync();
+  print('Informe sua idade:');
+  dynamic entradaIdade = stdin.readLineSync();
+  dynamic idade = int.parse(entradaIdade);
+  print('Informe seu salario:');
+  dynamic entradaSalario = stdin.readLineSync();
+  dynamic salario = double.parse(entradaSalario);
+
+  print('Ola! Meu nome e $nome tenho $idade anos e meu salario de $salario');
 }
